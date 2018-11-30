7ce4f7749cb2e740fb11c437c32a93871ade5f3a
xizige <381144235@qq.com>
2018-11-30 10:02:53 +0800

    修改构造器

--- src/main/java/com/zsk/pojo/Document.java
+++ src/main/java/com/zsk/pojo/Document.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.io.Serializable;
 import java.util.Date;
 
--- src/main/java/com/zsk/pojo/TagTable.java
+++ src/main/java/com/zsk/pojo/TagTable.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Data
 public class TagTable implements Serializable{
     @Id
-    private String  tagId;
+    private Integer  tagId;
     private String  tagText;
     private Date    createTime;
     
